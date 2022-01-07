38404144ed1f75a66bd3ca53a8ac8b7b3aa2c750
Bharghava Varun A <abvarun226@gmail.com>
2022-01-07 16:32:12 +0530

    update sw

--- sw.js
+++ sw.js
@@ -1,10 +1,9 @@
-var cacheName = 'ayada:time:v2:static';
+var cacheName = 'ayada:time:v4:static';
 
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             return cache.addAll([
-                './',
                 './images/alarm-clock-24.png',
                 './images/alarm-clock-144.png',
                 './css/style.css',
